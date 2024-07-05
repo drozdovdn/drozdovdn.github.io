684c8103756dea0d56667bc4d7be202e9feaa1a3
drozdov <drozdov.dn29@gmail.com>
2024-07-05 23:37:19 +0300

    init desctop start version

---
+++ src/helpers.ts
@@ -0,0 +1,28 @@
+import type { LinkProps } from './components/types.ts'
+
+export const linksNavBar: LinkProps[] = [
+	{
+		href: '/',
+		title: 'Главная'
+	},
+	{
+		href: '/about',
+		title: 'Обо мне'
+	},
+	{
+		href: '/blog',
+		title: 'Блог'
+	},
+	{
+		href: '/posts/post-1',
+		title: 'Пост 1'
+	},
+	{
+		href: '/posts/post-2',
+		title: 'Пост 2'
+	},
+	{
+		href: '/posts/post-3',
+		title: 'Пост 3'
+	}
+]
