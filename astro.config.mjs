923c001d2e463a02940c4cbdfc98a2281c257a10
drozdov <drozdov.dn29@gmail.com>
2024-07-15 11:51:14 +0300

    add yml and fix code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+// import tailwind from '@astrojs/tailwind';
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  site: 'https://drozdovdn.github.io',
+  // base: 'drozdovdn.github.io',
+  integrations: [ react()],
+
 });
